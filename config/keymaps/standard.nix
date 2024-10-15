7751577eae104c1bc09ab831f3cea00415941715
Answare <57868378+4nZwar3@users.noreply.github.com>
2024-10-14 21:19:58 -0600

    Update standard.nix

--- config/keymaps/standard.nix
+++ config/keymaps/standard.nix
@@ -103,4 +103,12 @@ in
     action = ">gv";
     options.desc = "Indent line";
   }
+  {
+    mode = "n";
+    key = "<leader>gg";
+    action = "<cmd>LazyGit<CR>";
+    options = {
+      desc = "LazyGit (root dir)";
+    };
+  }
 ]
